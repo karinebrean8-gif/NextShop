e31c72b92012b885f0651ea4b0bf7aabb74e88b8
Shakib Mia <karinebrean8@gmail.com>
2026-07-25 01:07:32 +0600

    Reformat registry.py for consistent style

--- Nextshop/payment/registry.py
+++ Nextshop/payment/registry.py
@@ -6,19 +6,17 @@ from .strategies import (
     BkashStrategy, BinanceCryptoStrategy, CodStrategy
 )
 
-# Instantiating the singletons
 _stripe = StripeStrategy()
 _visa = VisaDirectStrategy()
 _bkash = BkashStrategy()
 _binance = BinanceCryptoStrategy()
 _cod = CodStrategy()
 
-# The Hyper-Fast Routing Matrix Dictionary
 GATEWAY_ROUTING_MATRIX: Final[Dict[str, BasePaymentStrategy]] = {
     "CARD_STRIPE": _stripe,
     "CARD_VISA_DIRECT": _visa,
     "MFS_BKASH": _bkash,
     "CRYPTO_BINANCE": _binance,
     "OFFLINE_COD": _cod,
-    # "CARD_PAYPAL": PaypalStrategy(), etc.
+   
 }
